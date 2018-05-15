8a3cf132a2cb3a5a0af2fd9aec2c8bddd34c4d0d
Chip Salim <ilusi@yahoo.com>
2018-05-15 10:31:17 +0700

    fixed typos

---
+++ index.js
@@ -0,0 +1,36 @@
+var path = require('path')
+var funcs = require('./funcs')
+var encodeName = funcs.encodeName
+
+var session = {
+  username: process.argv[2],
+  lastMessageHash: process.argv[3]
+}
+
+if (!session.username || !session.lastMessageHash) {
+  console.log('Usage: node index.js <username> <hash>')
+  process.exit(0)
+}
+
+// 1. load the database
+var dbFile = path.join(__dirname, 'db', 'index.json')
+// console.log('dbFile', dbFile)
+
+funcs.loadDb(dbFile, function (err, db) {
+// console.log('db', db)
+
+    // 2. encode the name
+    var encoded = encodeName(session.username)
+console.log('encoded', encoded)
+
+  // 3. find the user's inbox
+  var inbox = funcs.findInbox(db, encoded)
+console.log('inbox', inbox)
+
+  // 4. find the next message
+  var nextMessage = funcs.findNextMessage(inbox, session.lastMessageHash)
+
+  // 5. print out the message.
+  // Paste the console output into the "Solution" field and you're done!
+  console.log(nextMessage)
+})
